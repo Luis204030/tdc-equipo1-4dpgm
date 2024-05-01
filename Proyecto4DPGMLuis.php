0d71619f7c92a7cf73c6fd58bb24e6574efa205c
TilinInsanoVtuber <161766784+TilinInsanoVtuber@users.noreply.github.com>
2024-04-30 19:36:46 -0700

    Update Proyecto4DPGMLuis.php

--- Proyecto4DPGMLuis.php
+++ Proyecto4DPGMLuis.php
@@ -35,15 +35,13 @@
     
     function showtime() {
         var now = new Date();
-        varhours = now.getHours();
+        var hours = now.getHours();
         var minutes = now.getMinutes();
         var seconds = now.getSeconds()
         var timeValue = ""+((hours>12)?
         hours-12:hours)
-        timeValue+=((minutes<10)?
-        ":0":":")+minutes
-        timeValue+=((seconds<10)?
-        ":0":":")+seconds
+        timeValue+=((minutes<10)? ":0":":")+minutes
+        timeValue+=((seconds<10)? ":0":":")+seconds
         timeValue+=(hours>=12)?"P.M.":"A.M."
         document.clock.face.value=
         timeValue;
